d2a2ea392dfb00fa95b8fd27f297a95bc250bb04
Brindia Joseph <brindiatyjuana@gmail.com>
2022-04-05 23:59:10 -0500

    commit tree 8e311d77230cecbf88aa5ccfd9eb62197b71c7eb

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+// code your solution here// use string interp to 
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}// replace rollerskate with `${activity} so it can be replaced console.log to see output
+
+  let mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+  }
+  
+  let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
+  
+  
--- test/indexTest.js
+++ test/indexTest.js
@@ -29,17 +29,17 @@ describe("index.js", () => {
 
   describe("defines wrapAdjective function according to the specification", function() {
     it("function exists", function() {
-      expect(wrapAdjective).to.exist
+      expect(adjective).to.exist
     })
     
     it("when initialized with '*' creates a function that, when called, wraps an adjective in a highlight", function() {
-      let result = wrapAdjective('*')
+      let result = adjective('*')
       let emphatic = result("a hard worker")
       expect(emphatic).to.equal("You are *a hard worker*!")
     });
 
     it("when initialized with '||' creates a function that, when called, wraps an adjective in a highlight", function() {
-      let result = wrapAdjective("||")
+      let result = adjective("||")
       let emphatic = result("a dedicated programmer")
       expect(emphatic).to.equal("You are ||a dedicated programmer||!")
     });
